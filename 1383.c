88f97ce17398a493fb72cfb6a4915a621210b0b9
LMeira0 <lmeira@unifesp.br>
2026-08-26 18:20:59 -0300

    Add Sudoku validation logic in 1383.c
    Implement Sudoku validation for multiple instances.

---
+++ 1383.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int x[9][9];
+
+int valido(int a[9]){
+    int visto[10] = {0};
+    for (int i = 0; i < 9; i++) {
+        int v = a[i];
+        if (v < 1 || v > 9 || visto[v]) {
+            return 0;
+        }
+        visto[v] = 1;
+    }
+    return 1;
+}
+int main() {
+    int n, ok = 1;
+    scanf("%d", &n);
+
+    for(int f = 0; f < n; f++) {
+
+        for(int i = 0; i <  9; i++) {
+            for(int j = 0; j < 9; j++) {
+                scanf("%d", &x[i][j]);
+            }
+        }
+        for(int i = 0; i < 9; i++) {
+            ok = valido(x[i]);
+        }
+        for(int j = 0; j < 9 && ok; j++) {
+            int coluna[9];
+            for(int i = 0; i < 9; i++) {
+                coluna[i] = x[i][j];
+                ok = valido(coluna);
+            }
+        }     
+        for(int i = 0; i < 9 && ok; i += 3) {
+            for(int j = 0; j < 9 && ok; j += 3) {
+                int bloco[9], r = 0;
+                for(int k = 0; k < 3; k++) {
+                    for(int l = 0; l < 3; l++) {
+                        bloco[r++] = x[i + k][j + l];
+                    }
+                }
+                ok = valido(bloco);
+            }
+        }
+        printf("Instancia %d\n", f + 1);
+        if(ok) {
+            printf("SIM\n\n");
+        } else {
+            printf("NAO\n\n");
+        }
+    }
+    return 0;
+}
