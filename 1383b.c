3d74b042ef67845057456aa9b58a2354860f325a
LMeira0 <lmeira@unifesp.br>
2026-09-18 13:03:10 -0300

    Add header comments for program metadata
    Added header comments to describe the program's purpose and details.

--- 1383b.c
+++ 1383b.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Leonardo Freire Meira
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1383
+Data        : 20/08/2026
+Objetivo    : Testar se a  sequencia do sudoku esta certa.
+Dificuldade : Pensar na logica.
+Uso de IA   : Ajudar a ver todas as partes que tinham que mudar.
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <stdlib.h>
 int valido(int *a){
