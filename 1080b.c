27fb315b51866ffa6d9b6129f503a795dc75c86f
LMeira0 <lmeira@unifesp.br>
2026-09-18 13:00:19 -0300

    Add header comments to 1080b.c
    Added header comments with problem details and author information.

--- 1080b.c
+++ 1080b.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Leonardo Freire Meira
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1383
+Data        : 20/08/2026
+Objetivo    : Ler 100 numeros e dizer qual eh o maior e sua posicao desde que sejam inteiros, positivos e distintos.
+Dificuldade : Nenhum
+Uso de IA   : Nao
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <stdlib.h>
  
